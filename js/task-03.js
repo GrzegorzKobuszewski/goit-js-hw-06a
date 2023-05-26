47a393841f6453c7fe6368e42ccf4ef9a64fee10
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2023-05-26 23:15:37 +0200

    Task 03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,60 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Stworzenie galerii z nowych zdjęć
+
+const galleryList = document.querySelector("ul.gallery");
+
+const newElements = images
+  .map(image => 
+    `<li class = "gallery-item">
+      <img class = "gallery-picture" src = "${image.url}" alt = "${image.alt}" />
+    </li>`)
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", newElements);
+console.log(galleryList);
+
+// Sprawdzenie tego, co wyświetla się na konsoli
+// console.log(galleryList.innerHTML);
+
+
+// Stylowanie "starej" galerii z "nowymi" elemetami
+
+galleryList.style.display = "flex";
+// galleryList.style.flexDirection = "column-reverse";
+galleryList.style.gap = "20px";
+galleryList.style.listStyle = "none";
+galleryList.style.padding = "0px";
+
+
+// Stylowanie nowych zdjeć w "starej" galerii
+
+const newGalleryItems = document.querySelectorAll("ul.gallery > li.gallery-item");
+const newGalleryPictures = document.querySelectorAll("ul.gallery > li.gallery-item > img.gallery-picture");
+
+newGalleryPictures.forEach(e => {
+  e.setAttribute("alt", `${e.getAttribute("alt")}, new gallery photo`);
+  e.setAttribute("width", "400px");
+  e.classList.add("newGalleryPicture");
+  e.style.borderRadius = "40px";
+  e.style.hover = "cursor: pointer";
+  e.style.transition = "1000ms linear";
+})
+
+
+
+
+// const galleryList = document.querySelector("ul.gallery");
+
+// const galleryWithNewElements = images
+//   .forEach(image => { 
+//     galleryList.insertAdjacentHTML("beforeend",
+//     `<li>
+//       <img src="${image.url}" alt="${image.alt}" width="300"/>
+//     </li>`)
+//   });
+
+// console.log(galleryWithNewElements);
+// console.log(galleryList.innerHTML);
