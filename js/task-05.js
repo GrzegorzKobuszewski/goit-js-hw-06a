1ba9fc0383eace5454fe484684dddd2eaeea540f
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2023-05-27 16:32:41 +0200

    Task 07

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("input#name-input");
+const output = document.querySelector("span#name-output");
+
+console.log(input.innerHTML);
+console.log(output.innerHTML);
+
+let inputValue = "Stranger";
+
+input.addEventListener("input", (e) => {
+    if (e.currentTarget.value === "") output.textContent = inputValue
+    else output.textContent = e.currentTarget.value;
+})
+
