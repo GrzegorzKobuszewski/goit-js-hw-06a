9c13d3036904ac136aa35a665dea724c27b8e386
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2023-05-29 23:17:40 +0200

    Last changes

--- js/task-10.js
+++ js/task-10.js
@@ -1,21 +1,24 @@
 const createButton = document.querySelector(`button[data-create=""]`);
 const destroyButton = document.querySelector(`button[data-destroy=""]`);
-const input = document.querySelector(`input`);
-const boxes = document.querySelector(`#boxes`);
+const input = document.querySelector(`div#controls>input`);
+const boxes = document.querySelector(`div#boxes`);
 
-createButton = addEventListener( "click", () => createBoxes(input.value) );
-destroyButton = addEventListener( "click", () => destroyBoxes() );
+createButton.addEventListener( "click", () => createBoxes(input.value) );
+destroyButton.addEventListener( "click", () => destroyBoxes() );
 
 function createBoxes(number) {
+  
   let boxHeight = 30;
   let boxWidth = 30;
+  let border = "solid black 2px";
 
   for (let i = 0; i < number; i += 1) {
     const box = document.createElement("div");
     box.classList.add("box");
     box.style.backgroundColor = `${getRandomHexColor()}`;
     box.style.height = `${boxHeight+10*i}px`;
-    box.style.width = `${boxWidth+10*i}px`;
+    box.style.width = `${boxWidth + 10 * i}px`;
+    box.style.borderBottom = box.style.borderLeft = box.style.borderRight = `${border}`;
     boxes.prepend(box);
   }
 }
@@ -28,4 +31,4 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
