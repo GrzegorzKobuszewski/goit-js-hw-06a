63e88c7cdf5ae349bd9c0ea1667b53d00f4233a0
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2023-06-13 02:24:33 +0200

    Ostatnie poprawki

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,25 @@
 const input = document.querySelector("input#validation-input");
 
 input.addEventListener("blur", (e) => {
-    if ( e.currentTarget.value.length === parseInt(e.currentTarget.getAttribute("data-length")) ) e.currentTarget.setAttribute("class", "valid")
-    else e.currentTarget.setAttribute("class", "invalid");
+  if (e.currentTarget.value.length === parseInt(e.currentTarget.getAttribute("data-length")))
+  {
+    e.currentTarget.classList.add("valid")
+    e.currentTarget.classList.remove("invalid")
+  }
+  else {
+    e.currentTarget.classList.add("invalid")
+    e.currentTarget.classList.remove("valid")
+  }
 })
 
 console.log(parseInt(input.getAttribute("data-length")));
+
+// To rozwiązanie ma w kodzie 
+// input.addEventListener("blur", (e) => {
+//     if ( e.currentTarget.value.length === parseInt(e.currentTarget.getAttribute("data-length")) ) 
+//     e.currentTarget.setAttribute("class", "valid")
+//     else e.currentTarget.setAttribute("class", "invalid");
+// })
 
 /*
 Rozwiązanie Łukasza, ale lepiej nie używać z this... bo jak ktoś zmieni nazwę funkcji, albo zrobi z tego strzałkową to funkcja straci kontekst...
